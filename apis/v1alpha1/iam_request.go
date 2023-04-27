cee63281631fbb6eedc021a4caeb0a4b92e87fec
Suhong Qin <51539171+sqin2019@users.noreply.github.com>
2023-04-27 15:14:17 -0700

    feat: iam request definition (#3)
    gcp policy type:
    https://cloud.google.com/resource-manager/reference/rest/Shared.Types/Policy
    will add a sample yaml file documentation in the future.

---
+++ apis/v1alpha1/iam_request.go
@@ -0,0 +1,44 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package v1alpha1
+
+// IAMRequest represents a request to update IAM policies.
+type IAMRequest struct {
+	// List of ResourcePolicy, each specifies the IAM principals/members to role
+	// bindings to be added for a GCP resource IAM policy.
+	ResourcePolicies []*ResourcePolicy `yaml:"policies,omitempty"`
+}
+
+// ResourcePolicy specifies the IAM principals/members to role bindings to be
+// added for a GCP resource IAM policy.
+type ResourcePolicy struct {
+	// Resource represents one of GCP organization, folder, and project.
+	Resource string `yaml:"resource,omitempty"`
+
+	// Bindings contains a list of IAM principals/members to role bindings.
+	Bindings []*Binding `yaml:"bindings,omitempty"`
+}
+
+// Binding associates IAM principals/members with a role.
+type Binding struct {
+	// Members is a list of IAM principals, check
+	// https://cloud.google.com/resource-manager/reference/rest/Shared.Types/Binding
+	// for acceptable values.
+	Members []string `yaml:"members,omitempty"`
+
+	// Role to be assigned to Members. For example, roles/viewer, roles/editor, or
+	// roles/owner.
+	Role string `yaml:"role,omitempty"`
+}
