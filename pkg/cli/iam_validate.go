942db48730d9cd9bc39a1895a8deee0536531944
Suhong Qin <suhongq@google.com>
2023-05-19 18:02:06 +0000

    feat: add validate cli

---
+++ pkg/cli/iam_validate.go
@@ -0,0 +1,95 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/abcxyz/access-on-demand/apis/v1alpha1"
+	"github.com/abcxyz/access-on-demand/pkg/requestutil"
+	"github.com/abcxyz/pkg/cli"
+)
+
+var _ cli.Command = (*IAMValidateCommand)(nil)
+
+// IAMValidateCommand validates IAM requests.
+type IAMValidateCommand struct {
+	cli.BaseCommand
+
+	flagPath string
+}
+
+func (c *IAMValidateCommand) Desc() string {
+	return `Validate the IAM request YAML file at the given path`
+}
+
+func (c *IAMValidateCommand) Help() string {
+	return `
+Usage: {{ COMMAND }} [options]
+
+Validate the IAM request YAML file at the given path:
+
+      aod iam validate -path "/path/to/file.yaml"
+`
+}
+
+func (c *IAMValidateCommand) Flags() *cli.FlagSet {
+	set := cli.NewFlagSet()
+
+	// Command options
+	f := set.NewSection("COMMAND OPTIONS")
+
+	f.StringVar(&cli.StringVar{
+		Name:    "path",
+		Target:  &c.flagPath,
+		Example: "/path/to/file.yaml",
+		Usage:   `The path of IAM request file, in YAML format.`,
+	})
+
+	return set
+}
+
+func (c *IAMValidateCommand) Run(ctx context.Context, args []string) error {
+	f := c.Flags()
+	if err := f.Parse(args); err != nil {
+		return fmt.Errorf("failed to parse flags: %w", err)
+	}
+	args = f.Args()
+	if len(args) > 0 {
+		return fmt.Errorf("unexpected arguments: %q", args)
+	}
+
+	if c.flagPath == "" {
+		return fmt.Errorf("path is required")
+	}
+
+	return c.validate(ctx)
+}
+
+func (c *IAMValidateCommand) validate(ctx context.Context) error {
+	// Read request from YAML file.
+	req, err := requestutil.ReadFromPath(c.flagPath)
+	if err != nil {
+		return fmt.Errorf("failed to read %T: %w", req, err)
+	}
+
+	if err := v1alpha1.ValidateIAMRequest(req); err != nil {
+		return fmt.Errorf("failed to validate %T: %w", req, err)
+	}
+	c.Outf("Successfully validated IAM request")
+
+	return nil
+}
---
+++ pkg/requestutil/request_test.go
@@ -0,0 +1,149 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package requestutil
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/abcxyz/access-on-demand/apis/v1alpha1"
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/go-cmp/cmp"
+	"google.golang.org/protobuf/testing/protocmp"
+)
+
+func TestIAMValidateCommand(t *testing.T) {
+	t.Parallel()
+
+	// Set up IAM request file.
+	requestFileContentByName := map[string]string{
+		"valid.yaml": `
+policies:
+- resource: organizations/foo
+  bindings:
+  - members:
+    - user:test-org-userA@example.com
+    - user:test-org-userB@example.com
+    role: roles/cloudkms.cryptoOperator
+  - members:
+    - user:test-org-userA@example.com
+    - user:test-org-userB@example.com
+    role: roles/accessapproval.approver
+- resource: folders/bar
+  bindings:
+    - members:
+      - user:test-folder-user@example.com
+      role: roles/cloudkms.cryptoOperator
+- resource: projects/baz
+  bindings:
+    - members:
+      - user:test-project-user@example.com
+      role: roles/bigquery.dataViewer
+`,
+		"invalid.yaml": `bananas`,
+	}
+	dir := t.TempDir()
+	for name, content := range requestFileContentByName {
+		path := filepath.Join(dir, name)
+		if err := os.WriteFile(path, []byte(content), 0o600); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	cases := []struct {
+		name     string
+		path     string
+		expReq   *v1alpha1.IAMRequest
+		expErr   string
+	}{
+		{
+			name:    "success",
+			path:    filepath.Join(dir, "valid.yaml"),
+			expReq:  &v1alpha1.IAMRequest{
+				ResourcePolicies: []*v1alpha1.ResourcePolicy{
+					{
+						Resource: "organizations/foo",
+						Bindings: []*v1alpha1.Binding{
+							{
+								Members: []string{
+									"user:test-org-userA@example.com",
+									"user:test-org-userB@example.com",
+								},
+								Role: "roles/cloudkms.cryptoOperator",
+							},
+							{
+								Members: []string{
+									"user:test-org-userA@example.com",
+									"user:test-org-userB@example.com",
+								},
+								Role: "roles/accessapproval.approver",
+							},
+						},
+					},
+					{
+						Resource: "folders/bar",
+						Bindings: []*v1alpha1.Binding{
+							{
+								Members: []string{
+									"user:test-folder-user@example.com",
+								},
+								Role: "roles/cloudkms.cryptoOperator",
+							},
+						},
+					},
+					{
+						Resource: "projects/baz",
+						Bindings: []*v1alpha1.Binding{
+							{
+								Members: []string{
+									"user:test-project-user@example.com",
+								},
+								Role: "roles/bigquery.dataViewer",
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			name:    "invalid_path",
+			path:    "foo",
+			expErr:  `failed to read file at "foo"`,
+		},
+		{
+			name:    "invalid_yaml",
+			path:    filepath.Join(dir, "invalid.yaml"),
+			expErr:  "failed to unmarshal yaml to v1alpha1.IAMRequest",
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			req, err := ReadFromPath(tc.path)
+			if diff := testutil.DiffErrString(err, tc.expErr); diff != "" {
+				t.Errorf("Process(%+v) got error diff (-want, +got):\n%s", tc.name, diff)
+			}
+
+			if diff := cmp.Diff(tc.expReq, req, protocmp.Transform()); diff != "" {
+				t.Errorf("Process(%+v) got request diff (-want, +got): %v", tc.name, diff)
+			}
+		})
+	}
+}
