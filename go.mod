64ddcf9aee305ffd26365e57dfc3fc58b5c5c6dd
Suhong Qin <51539171+sqin2019@users.noreply.github.com>
2023-05-23 09:38:51 -0700

    feat: cli handle iam request (#12)
    fix #11

--- go.mod
+++ go.mod
@@ -3,28 +3,32 @@ module github.com/abcxyz/access-on-demand
 go 1.20
 
 require (
-	cloud.google.com/go/iam v0.12.0
-	cloud.google.com/go/resourcemanager v1.5.0
+	cloud.google.com/go/iam v0.13.0
+	cloud.google.com/go/resourcemanager v1.7.0
 	github.com/abcxyz/pkg v0.3.1-0.20230410214104-4b190bd00925
 	github.com/google/go-cmp v0.5.9
-	github.com/googleapis/gax-go/v2 v2.7.0
+	github.com/googleapis/gax-go/v2 v2.8.0
 	github.com/sethvargo/go-retry v0.2.4
-	google.golang.org/api v0.110.0
-	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4
-	google.golang.org/grpc v1.53.0
+	google.golang.org/api v0.118.0
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1
+	google.golang.org/grpc v1.55.0
 	google.golang.org/protobuf v1.30.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
-	cloud.google.com/go v0.110.0 // indirect
-	cloud.google.com/go/compute v1.18.0 // indirect
-	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/compute v1.19.0 // indirect
 	cloud.google.com/go/longrunning v0.4.1 // indirect
+	github.com/google/s2a-go v0.1.0 // indirect
+)
+
+require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
 	github.com/benbjohnson/clock v1.3.0 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.1.0 // indirect
 	github.com/goccy/go-json v0.10.1 // indirect
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
 	github.com/kr/text v0.1.0 // indirect
@@ -40,9 +44,9 @@ require (
 	go.uber.org/multierr v1.10.0 // indirect
 	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/oauth2 v0.5.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/oauth2 v0.7.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 )
---
+++ pkg/cli/iam_handle.go
@@ -0,0 +1,170 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cli
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/abcxyz/access-on-demand/apis/v1alpha1"
+	"github.com/abcxyz/access-on-demand/pkg/handler"
+	"github.com/abcxyz/pkg/cli"
+	"gopkg.in/yaml.v3"
+
+	resourcemanager "cloud.google.com/go/resourcemanager/apiv3"
+)
+
+var _ cli.Command = (*IAMHandleCommand)(nil)
+
+// iamHandler interface that handles the IAMRequestWrapper.
+type iamHandler interface {
+	Do(context.Context, *v1alpha1.IAMRequestWrapper) ([]*v1alpha1.IAMResponse, error)
+}
+
+// IAMHandleCommand handles IAM requests.
+type IAMHandleCommand struct {
+	cli.BaseCommand
+
+	flagPath string
+
+	flagDuration time.Duration
+
+	// TODO(#13): add start time flag.
+
+	// testHandler is used for testing only.
+	testHandler iamHandler
+}
+
+func (c *IAMHandleCommand) Desc() string {
+	return `Handle the IAM request YAML file in the given path`
+}
+
+func (c *IAMHandleCommand) Help() string {
+	return `
+Usage: {{ COMMAND }} [options]
+
+Handle the IAM request YAML file in the given path:
+
+      aod iam handle -path "/path/to/file.yaml" -duration "2h"
+`
+}
+
+func (c *IAMHandleCommand) Flags() *cli.FlagSet {
+	set := cli.NewFlagSet()
+
+	// Command options
+	f := set.NewSection("COMMAND OPTIONS")
+
+	f.StringVar(&cli.StringVar{
+		Name:    "path",
+		Target:  &c.flagPath,
+		Example: "/path/to/file.yaml",
+		Usage:   `The path of IAM request file, in YAML format.`,
+	})
+
+	f.DurationVar(&cli.DurationVar{
+		Name:    "duration",
+		Target:  &c.flagDuration,
+		Example: "2h",
+		Usage:   `The IAM permission lifecycle, as a duration.`,
+	})
+
+	return set
+}
+
+func (c *IAMHandleCommand) Run(ctx context.Context, args []string) error {
+	f := c.Flags()
+	if err := f.Parse(args); err != nil {
+		return fmt.Errorf("failed to parse flags: %w", err)
+	}
+	args = f.Args()
+	if len(args) > 0 {
+		return fmt.Errorf("unexpected arguments: %q", args)
+	}
+
+	if c.flagPath == "" {
+		return fmt.Errorf("path is required")
+	}
+
+	if c.flagDuration <= 0 {
+		return fmt.Errorf("a positive duration is required")
+	}
+
+	return c.handleIAM(ctx)
+}
+
+func (c *IAMHandleCommand) handleIAM(ctx context.Context) error {
+	// Unmarshal the YAML file to IAMRequest
+	// TODO(#14): use limit reader.
+	data, err := os.ReadFile(c.flagPath)
+	if err != nil {
+		return fmt.Errorf("failed to read IAM request file at %q, %w", c.flagPath, err)
+	}
+	var req v1alpha1.IAMRequest
+	if err := yaml.Unmarshal(data, &req); err != nil {
+		return fmt.Errorf("failed to unmarshal yaml to %T: %w", req, err)
+	}
+
+	var h iamHandler
+	if c.testHandler != nil {
+		// Use testHandler if it is for testing.
+		h = c.testHandler
+	} else {
+		// Create resource manager clients.
+		organizationsClient, err := resourcemanager.NewOrganizationsClient(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to create organizations client: %w", err)
+		}
+		defer organizationsClient.Close()
+
+		foldersClient, err := resourcemanager.NewFoldersClient(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to create folders client: %w", err)
+		}
+		defer foldersClient.Close()
+
+		projectsClient, err := resourcemanager.NewProjectsClient(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to create projects client: %w", err)
+		}
+		defer projectsClient.Close()
+
+		// Create IAMHandler with the clients.
+		h, err = handler.NewIAMHandler(
+			ctx,
+			organizationsClient,
+			foldersClient,
+			projectsClient,
+		)
+		if err != nil {
+			return fmt.Errorf("failed to create IAM handler: %w", err)
+		}
+	}
+
+	// Wrap IAMRequest to include Duration.
+	reqWrapper := &v1alpha1.IAMRequestWrapper{
+		IAMRequest: &req,
+		Duration:   c.flagDuration,
+	}
+	// TODO(#15): add a log level to output handler response.
+	if _, err := h.Do(ctx, reqWrapper); err != nil {
+		return fmt.Errorf("failed to handle IAM request: %w", err)
+	}
+	c.Outf("Successfully handled IAM request")
+
+	return nil
+}
