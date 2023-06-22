0915f42318014c417f65aff434485639b8408481
Suhong Qin <suhongq@google.com>
2023-06-22 21:27:13 +0000

    feat: CLI request handler

---
+++ pkg/handler/cli_handler.go
@@ -0,0 +1,95 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// handler package that handles AOD request.
+package handler
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+
+	"github.com/abcxyz/access-on-demand/apis/v1alpha1"
+)
+
+// CLIHandler runs cli commands in the CLIRequest.
+type CLIHandler struct {
+	// By default, stdout discards the command outputs, stderr is os.Stderr.
+	stdout, stderr io.Writer
+}
+
+// CLIHandlerOption is the option to set up an CLIHandler.
+type CLIHandlerOption func(h *CLIHandler) *CLIHandler
+
+// WithStderr sets the handler's stderr.
+func WithStderr(w io.Writer) CLIHandlerOption {
+	return func(h *CLIHandler) *CLIHandler {
+		h.stderr = w
+		return h
+	}
+}
+
+// WithDebugMode sets the handler's stdout to w.
+func WithDebugMode(w io.Writer) CLIHandlerOption {
+	return func(h *CLIHandler) *CLIHandler {
+		h.stdout = w
+		return h
+	}
+}
+
+// WithDefaultDebugMode sets the handler's stdout to os.Stdout.
+func WithDefaultDebugMode() CLIHandlerOption {
+	return func(h *CLIHandler) *CLIHandler {
+		h.stdout = os.Stdout
+		return h
+	}
+}
+
+// NewCLIHandler creates a new CLIHandler with provided options.
+func NewCLIHandler(ctx context.Context, opts ...CLIHandlerOption) *CLIHandler {
+	// Set default stderr.
+	h := &CLIHandler{stderr: os.Stderr}
+	for _, opt := range opts {
+		h = opt(h)
+	}
+	return h
+}
+
+// Do runs the do commands.
+func (h *CLIHandler) Do(ctx context.Context, r *v1alpha1.CLIRequest) error {
+	return h.run(r.CLI, r.Do)
+}
+
+// Cleanup runs the cleanup commands.
+func (h *CLIHandler) Cleanup(ctx context.Context, r *v1alpha1.CLIRequest) error {
+	return h.run(r.CLI, r.Cleanup)
+}
+
+func (h *CLIHandler) run(cli string, cmds []string) error {
+	for _, c := range cmds {
+		cmd := exec.Command(cli, c)
+		if h.stdout != nil {
+			cmd.Stdout = h.stdout
+		}
+		if h.stderr != nil {
+			cmd.Stderr = h.stderr
+		}
+		if err := cmd.Run(); err != nil {
+			return fmt.Errorf("failed to run command %q, error %w", c, err)
+		}
+	}
+	return nil
+}
---
+++ pkg/handler/cli_handler_test.go
@@ -0,0 +1,167 @@
+// Copyright 2023 The Authors (see AUTHORS file)
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// handler package that handles AOD request.
+package handler
+
+import (
+	"bytes"
+	"context"
+	"strings"
+	"testing"
+
+	"github.com/abcxyz/access-on-demand/apis/v1alpha1"
+	"github.com/abcxyz/pkg/testutil"
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestCLIHandlerDo(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name           string
+		request        *v1alpha1.CLIRequest
+		expErrSubstr   string
+		expOutErr      string
+		expOutResponse string
+	}{
+		{
+			name: "success",
+			request: &v1alpha1.CLIRequest{
+				CLI: "echo",
+				Do: []string{
+					"test do1",
+					"test do2",
+				},
+			},
+			expOutResponse: "test do1\ntest do2",
+		},
+		{
+			name: "invalid_cli_command",
+			request: &v1alpha1.CLIRequest{
+				CLI: "invalid",
+				Do: []string{
+					"test do",
+				},
+			},
+			expErrSubstr: `failed to run command "test do"`,
+		},
+		{
+			name: "failed_to_execute_cli_command",
+			request: &v1alpha1.CLIRequest{
+				CLI: "ls",
+				Do: []string{
+					"dir_not_exist",
+				},
+			},
+			expErrSubstr: `failed to run command "dir_not_exist"`,
+			expOutErr:    `ls: cannot access 'dir_not_exist': No such file or directory`,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+			stderr := bytes.NewBuffer(nil)
+			stdout := bytes.NewBuffer(nil)
+			h := NewCLIHandler(ctx, WithStderr(stderr), WithDebugMode(stdout))
+
+			// Run test.
+			gotErr := h.Do(ctx, tc.request)
+			if diff := testutil.DiffErrString(gotErr, tc.expErrSubstr); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error substring: %v", tc.name, diff)
+			}
+			if diff := cmp.Diff(strings.TrimSpace(tc.expOutErr), strings.TrimSpace(stderr.String())); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error output substring: %v", tc.name, diff)
+			}
+			if diff := cmp.Diff(strings.TrimSpace(tc.expOutResponse), strings.TrimSpace(stdout.String())); diff != "" {
+				t.Errorf("Process(%+v) got output response diff (-want, +got): %v", tc.name, diff)
+			}
+		})
+	}
+}
+
+func TestCLIHandlerCleanup(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		name           string
+		request        *v1alpha1.CLIRequest
+		expErrSubstr   string
+		expOutErr      string
+		expOutResponse string
+	}{
+		{
+			name: "success",
+			request: &v1alpha1.CLIRequest{
+				CLI: "echo",
+				Cleanup: []string{
+					"test do1",
+					"test do2",
+				},
+			},
+			expOutResponse: "test do1\ntest do2",
+		},
+		{
+			name: "invalid_cli_command",
+			request: &v1alpha1.CLIRequest{
+				CLI: "invalid",
+				Cleanup: []string{
+					"test do",
+				},
+			},
+			expErrSubstr: `failed to run command "test do"`,
+		},
+		{
+			name: "failed_to_execute_cli_command",
+			request: &v1alpha1.CLIRequest{
+				CLI: "ls",
+				Cleanup: []string{
+					"dir_not_exist",
+				},
+			},
+			expErrSubstr: `failed to run command "dir_not_exist"`,
+			expOutErr:    `ls: cannot access 'dir_not_exist': No such file or directory`,
+		},
+	}
+
+	for _, tc := range cases {
+		tc := tc
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := context.Background()
+			stderr := bytes.NewBuffer(nil)
+			stdout := bytes.NewBuffer(nil)
+			h := NewCLIHandler(ctx, WithStderr(stderr), WithDebugMode(stdout))
+
+			// Run test.
+			gotErr := h.Cleanup(ctx, tc.request)
+			if diff := testutil.DiffErrString(gotErr, tc.expErrSubstr); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error substring: %v", tc.name, diff)
+			}
+			if diff := cmp.Diff(strings.TrimSpace(tc.expOutErr), strings.TrimSpace(stderr.String())); diff != "" {
+				t.Errorf("Process(%+v) got unexpected error output substring: %v", tc.name, diff)
+			}
+			if diff := cmp.Diff(strings.TrimSpace(tc.expOutResponse), strings.TrimSpace(stdout.String())); diff != "" {
+				t.Errorf("Process(%+v) got output response diff (-want, +got): %v", tc.name, diff)
+			}
+		})
+	}
+}
