2fdaeb6bb996d35c7f69df8a399d1f0e4c79215b
Suhong Qin <suhongq@google.com>
2023-09-15 11:04:57 -0700

    fix: output correct executed commands

--- pkg/handler/tool_handler_test.go
+++ pkg/handler/tool_handler_test.go
@@ -47,10 +47,10 @@ func TestToolHandlerDo(t *testing.T) {
 			},
 			stdout: bytes.NewBuffer(nil),
 			expOutResponse: `
-bash -c "echo test do1"
+bash -c echo test do1
 test do1
 
-bash -c "echo test do2"
+bash -c echo test do2
 test do2
 `,
 		},
@@ -75,16 +75,16 @@ test do2
 			expHandleErrSubStr: "failed to parse cmd",
 		},
 		{
-			name: "fail_to_run",
+			name: "chained_commands",
 			request: &v1alpha1.ToolRequest{
 				Tool: "echo",
 				Do: []string{
-					`test1 && test2`,
+					`test1; echo test2`,
 				},
 			},
 			stdout: bytes.NewBuffer(nil),
 			expOutResponse: `
-echo test1 && test2
+echo test1
 test1`,
 		},
 		{
@@ -169,10 +169,10 @@ func TestToolHandlerCleanup(t *testing.T) {
 			},
 			stdout: bytes.NewBuffer(nil),
 			expOutResponse: `
-bash -c "echo test cleanup1"
+bash -c echo test cleanup1
 test cleanup1
 
-bash -c "echo test cleanup2"
+bash -c echo test cleanup2
 test cleanup2
 `,
 		},
@@ -197,16 +197,16 @@ test cleanup2
 			expHandleErrSubStr: "failed to parse cmd",
 		},
 		{
-			name: "fail_to_run",
+			name: "chained_commands",
 			request: &v1alpha1.ToolRequest{
 				Tool: "echo",
 				Cleanup: []string{
-					`test1 && test2`,
+					`test1 && echo test2`,
 				},
 			},
 			stdout: bytes.NewBuffer(nil),
 			expOutResponse: `
-echo test1 && test2
+echo test1
 test1`,
 		},
 		{
